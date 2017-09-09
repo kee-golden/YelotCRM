5e7c0bebb02c6163217423d30bbeb23a59b67bda
xyzloveabc <xyzloveabc@qq.com>
2017-09-09 19:48:19 +0800

    打印js多余删除

--- src/main/webapp/module-js/order/repair_order_my.js
+++ src/main/webapp/module-js/order/repair_order_my.js
@@ -85,67 +85,6 @@ require([ 'jquery', 'yaya', 'datatables.net' ], function($, yaya) {
 
                     },
                     yes: function (index) {
-//                    	alert(1);
-	                    /*var newstr = document.all.item("repairOrderDetail").innerHTML;//document.getElementById('repairOrderDetail').innerHTML;;
-	                    var oldstr = document.body.innerHTML;
-	                    document.body.innerHTML = newstr;
-	                    document.onload = function(){
-	                    	window.print();
-	                    	document.body.innerHTML = oldstr;
-		                    window.location.reload();
-	                    };*/
-//	                    window.print();
-//	                    document.body.innerHTML = oldstr;
-//	                    window.location.reload();
-//	                    return false;
-	                    
-	                    /*var t_img; // 定时器
-	                    var isLoad = true; // 控制变量（判断图片是否 加载完成）
-	                    isImgLoad(function(){//判断全部打印图片加载完成
-	                    	var newstr = document.all.item("repairOrderDetail").innerHTML;
-		                    document.body.innerHTML = newstr;
-		                    window.print(); 
-		                    window.location.reload();
-	                                // 加载完成
-	                            });
-	                    //判断图片加载的函数
-	                    function isImgLoad(callback){
-	                        // 查找所有打印图，迭代处理
-	                        $('.image').each(function(){
-	                            // 找到为0就将isLoad设为false，并退出each
-	                            if(this.height === 0){
-	                                isLoad = false;
-	                                return false;
-	                            }
-	                        });
-	                        // 为true，没有发现为0的。加载完毕
-	                        if(isLoad){
-	                            clearTimeout(t_img); // 清除定时器
-	                            // 回调函数
-	                            callback();
-	                        // 为false，因为找到了没有加载完成的图，将调用定时器递归
-	                        }else{
-	                            isLoad = true;
-	                            t_img = setTimeout(function(){
-	                                isImgLoad(callback); // 递归扫描
-	                            },500); // 我这里设置的是500毫秒就扫描一次，可以自己调整
-	                        }
-	                    }*/
-                    	
-                    	
-                    	/*<link href='/static/font-awesome/font-awesome.min.css' rel='stylesheet'>
-                    	<link href='/static/dataTables/css/dataTables.bootstrap.min.css' rel='stylesheet'>
-                    	<link href='/static/animate/animate.css' rel='stylesheet'>
-                    	<link rel='stylesheet' href='/static/ztree/css/yayaStyle/zTreeStyle.css' type='text/css'>
-                    	
-                    	
-                    	<link href='/static/dateTimePicker/css/jquery.datetimepicker.css' rel='stylesheet'>
-                    	<link href='/static/bootstrap-datetimepicker/css/bootstrap-datetimepicker.css' rel='stylesheet'>
-                    	<link rel='stylesheet' href='/static/css/iconfont.css'/>
-                    	<link href='/static/webuploader/webuploader.css' rel='stylesheet'>
-                    	<link href='/static/customer/customer.css' rel='stylesheet'>
-                    	<link href='/module-css/nav.css' rel='stylesheet'>*/
-	                    
                     	var headstr = "<html><head><link href='/static/bootstrap/css/bootstrap.min.css' rel='stylesheet'><link href='/static/yaya/css/style.css' rel='stylesheet'><link href='/module-css/basic.css' rel='stylesheet'></head><body>";
                     	var footstr = "</body></html>";  
                     	var newstr = document.all.item("repairOrderDetail").innerHTML;
@@ -153,7 +92,6 @@ require([ 'jquery', 'yaya', 'datatables.net' ], function($, yaya) {
                         printWindow.document.write(headstr + newstr + footstr);
                         return false;
                     }
-
                 });
             },
             error: function () {
